0d4a9eaa5186d5cbc92586c7728d2d49df8fc4c3
Frank Mathy <frankmathy@gmail.com>
2017-01-08 22:49:01 +0100

    First commit

--- Mini Golf/GameScene.swift
+++ Mini Golf/GameScene.swift
@@ -8,82 +8,44 @@
 
 import SpriteKit
 import GameplayKit
+import CoreGraphics
+import CoreImage
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    // Spielfeldgröße: 50 x 25 Blöcke
     
     override func didMove(to view: SKView) {
+        super.didMove(to: view)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        let myImage = UIImage(named: "elements")!
+        let croppedImage = GameScene.getElementFromImage(image: myImage, elementSize: 15, elementSpacing: 1, x: 0, y: 1)
+        let texture = SKTexture(image: croppedImage)
+        for y in -12..<13 {
+            for x in -24..<25 {
+                let sprite = SKSpriteNode(texture: texture)
+                sprite.position = CGPoint(x: CGFloat(x)*croppedImage.size.width, y: CGFloat(y)*croppedImage.size.width)
+                addChild(sprite)
+            }
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    /**
+     Crops an image to the specified size.
+        -parameter image: The image to be cropped.
+        -parameter toRect: The rect to which to crop.
+        -returns: The cropped image.
+    */
+    static func cropImage(image:UIImage, toRect rect:CGRect) -> UIImage{
+        let imageRef:CGImage = image.cgImage!.cropping(to: rect)!
+        let croppedImage:UIImage = UIImage(cgImage:imageRef)
+        return croppedImage
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    static func getElementFromImage(image: UIImage, elementSize: Int, elementSpacing: Int, x: Int, y: Int) -> UIImage {
+        let xCord: Int = 1 + ((elementSize + elementSpacing) * x)
+        let yCord: Int = 1 + ((elementSize + elementSpacing) * y)
+        let cropRect = CGRect(x: xCord, y: yCord, width: elementSize, height: elementSize)
+        return cropImage(image: image, toRect: cropRect)
     }
 }
